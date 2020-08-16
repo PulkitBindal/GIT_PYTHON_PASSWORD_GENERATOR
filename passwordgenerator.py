ff12d4323ca3e8cefd92324a7770b7906810329b
Pulkit <pulkit507shamli@gmail.com>
2020-08-16 20:57:41 +0530

    Updating README file

---
+++ passwordgenerator.py
@@ -0,0 +1,22 @@
+import string
+import random
+if __name__ == "__main__":
+    s1=string.ascii_lowercase
+    # print(s1)
+    s2=string.ascii_uppercase 
+    # print(s2)
+    s3=string.digits
+    # print(s3)
+    s4=string.punctuation
+    # print(s4)
+    plen=int(input("Enter the length of the password \n"))
+    s=[]
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+    random.shuffle(s)
+    random.shuffle(s)
+    print("Your strong password of length "+str(plen)+" is:")
+    print("".join(s[0:plen]))
+    
